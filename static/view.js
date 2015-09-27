4cc402b1767e6c0bb2beff67245450041030f6bb
Simon Walker <s.r.walker101@googlemail.com>
2015-09-27 19:50:53 +0100

    Add function for viewing lightcurve

--- static/view.js
+++ static/view.js
@@ -3,4 +3,15 @@ $(function() {
     fetchObjID(lc_id, function(obj_id) {
         $('h1#obj_id').text(obj_id);
     });
+
+    fetchLC(lc_id, function(data) {
+        var options = {
+            series: {
+                lines: { show: false, },
+                points: { show: true, },
+            },
+        };
+        $.plot('#lcplot', [data], options);
+    });
+
 });
