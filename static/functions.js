dc3e4d571d8eb55a856d1f639cc5c32c3db91b16
Simon Walker <s.r.walker101@googlemail.com>
2015-09-27 19:24:33 +0100

    Add error handling for functions

--- static/functions.js
+++ static/functions.js
@@ -1,12 +1,16 @@
 function fetchLC(data_index, callback) {
     $.getJSON('/lc/' + data_index, function(data) {
         callback(data);
+    }).fail(function() {
+        console.log('Cannot fetch lightcurve for object ' + data_index);
     });
 }
 
 function fetchObjID(data_index, callback) {
     $.getJSON('/obj_id/' + data_index, function(obj_id_data) {
         callback(obj_id_data.obj_id);
+    }).fail(function() {
+        console.log('Cannot fetch object id for object ' + data_index);
     });
 }
 
@@ -14,7 +18,11 @@ function fetchPositions(data_index, callback) {
     $.getJSON("/x/" + data_index, function(xdata) {
         $.getJSON("/y/" + data_index, function(ydata) {
             callback(xdata.data, ydata.data);
+        }).fail(function() {
+            console.log('Cannot fetch y position for object ' + data_index);
         });
+    }).fail(function() {
+        console.log('Cannot fetch x position for object ' + data_index);
     });
 }
 
