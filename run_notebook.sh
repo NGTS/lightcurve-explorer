29004dd4579da0f633333f72ab603b0a80c320b2
Simon Walker <s.r.walker101@googlemail.com>
2015-10-28 13:50:13 +0000

    Add script to run single notebook

---
+++ run_notebook.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+
+verify_args() {
+    if [[ "$#" != 2 ]]; then
+        echo "Usage: $0 <notebook> <output name>" >&2
+        exit 1
+    fi
+}
+
+main() {
+    verify_args "$@"
+    local readonly fname="$1"
+    local readonly outname="$2"
+    echo "Rendering notebook ${fname} to ${outname}"
+    
+    jupyter nbconvert --execute "${fname}" --to html --stdout > "${outname}"
+}
+
+main "$@"
