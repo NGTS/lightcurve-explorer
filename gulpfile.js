09f184a1eb77ea828a9b18cb479a5f64bd7aba00
Simon Walker <s.r.walker101@googlemail.com>
2015-11-01 18:35:34 +0000

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
 gulp.task('build', function() {
     browserify({
@@ -13,11 +14,13 @@ gulp.task('build', function() {
     .transform(babelify)
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('static/js'));
+    .pipe(gulp.dest('static/js'))
+    .pipe(livereload());
 });
 
 gulp.task('default', ['build']);
 
 gulp.task('watch', function() {
+    livereload.listen();
     gulp.watch('src/*.js', ['build']);
 });
