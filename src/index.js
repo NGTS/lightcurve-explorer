008e3a2b007700976171a53f5cde40d315dbb7f4
Simon Walker <s.r.walker101@googlemail.com>
2015-11-01 18:29:25 +0000

    Hook up simple index script

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <p>Hello world</p>
+        );
+    }
+});
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('visualiser')
+);
