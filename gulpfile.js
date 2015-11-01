af17dc9e737260a752a82a115b24c6b2fc03b82f
Simon Walker <s.r.walker101@googlemail.com>
2015-11-01 18:24:08 +0000

    Add build task for gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('build', function() {
+    browserify({
+        entries: 'src/index.js',
+        extensions: ['.js'],
+        debug: true,
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('static/js'));
+});
