231f3bcf9e601a892cb49e1101b6afa3c315ca8c
Simon Walker <s.r.walker101@googlemail.com>
2015-11-03 19:05:21 +0000

    Overwrite app.py with server.py

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask, jsonify, render_template
-import fitsio
-import numpy as np
-import argparse
-from scipy import stats
-from astropy.stats import sigma_clip
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+from tornado import gen
 import logging
+import concurrent.futures
+import fitsio
+from binmodule import fast_bin
+from astropy.stats import sigma_clip
 from astropy.coordinates import SkyCoord
 from astropy import units as u
-sys.path.append('.')
-
-from binmodule import fast_bin
+import numpy as np
+import joblib
+from scipy.stats import binned_statistic
+import argparse
 
 logging.basicConfig(
     level='INFO', format='[%(asctime)s] %(levelname)8s %(message)s')
 logger = logging.getLogger(__name__)
 
+executor = concurrent.futures.ThreadPoolExecutor()
+memory = joblib.Memory(cachedir='.tmp')
+
 SKIP = 30
-
-def fetch_from_fits(infile, hdu, index, skip=SKIP):
-    return infile[hdu][index:index + 1, skip:].ravel()
-
 
 def compute_extent(ts, percentile=5):
     '''
@@ -32,93 +35,54 @@ def compute_extent(ts, percentile=5):
     percentiles = np.percentile(ts, [percentile, 100 - percentile])
     return percentiles[-1] - percentiles[0]
 
+def fetch_from_fits(infile, hdu, index):
+    index = int(index)
+    return infile[hdu][index:index + 1, SKIP:].ravel()
 
-class VisualiseLightcurve(object):
+def bin_1d(flux, npts_per_bin, x=None):
+    x = x if x is not None else np.arange(flux.size)
+    bin_length = int(np.floor(flux.size / npts_per_bin))
+    by, be, _ = binned_statistic(x, flux, statistic='mean',
+                                        bins=bin_length)
+    return by, (be[:-1] + be[1:]) / 2.
 
-    def __init__(self, args):
-        self.filename = args.filename
-        self.hdu = args.hdu
+@memory.cache
+def extract_data(filename, npts_per_bin=None):
+    logger.info('Loading data')
+    with fitsio.FITS(filename) as infile:
+        flux = infile[args.hdu][:, SKIP:]
+
+    sc_flux = sigma_clip(flux, axis=1)
+
+    if npts_per_bin is not None:
+        sc_flux = fast_bin(sc_flux, npts_per_bin)
+
+    med_flux = np.median(sc_flux, axis=1)
+    mad_flux = np.median(np.abs(sc_flux - med_flux[:, np.newaxis]), axis=1)
+    std_flux = 1.48 * mad_flux
+    frms = std_flux / med_flux
+    ind = np.where((med_flux > 0) & (frms > 0))[0]
+    return ind, med_flux, frms
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def initialize(self, args, aperture_indexes, ind,
+                  frms, med_flux):
+        self.args = args
+        self.aperture_indexes = aperture_indexes
+        self.ind = ind
+        self.frms = frms
+        self.med_flux = med_flux
+
         self.npts_per_bin = args.bin
-        self.app = Flask(__name__)
+        self.filename = self.args.filename
 
-        self.app.add_url_rule('/', 'index', self.index)
-        self.app.add_url_rule('/view/<int:lc_id>', 'show',
-                              self.show_object)
-
-        self.app.add_url_rule('/api/data', 'frms', self.frms)
-        self.app.add_url_rule('/api/object_index/<int:lc_id>', 'object_index',
-                              self.fetch_object_index)
-        self.app.add_url_rule('/api/lc/<hdu>/<int:lc_id>', 'lc',
-                              self.fetch_lightcurve)
-        self.app.add_url_rule('/api/x/<int:lc_id>', 'x', self.fetch_x)
-        self.app.add_url_rule('/api/y/<int:lc_id>', 'y', self.fetch_y)
-        self.app.add_url_rule('/api/binning', 'binning', self.fetch_binning)
-        self.app.add_url_rule('/api/obj_id/<int:lc_id>', 'obj_id',
-                             self.fetch_obj_id)
-        self.app.add_url_rule('/api/sysrem_basis/<int:basis_id>',
-                              'sysrem_basis',
-                              self.fetch_sysrem_basis_functions)
-        self.app.add_url_rule('/api/xs/<int:lc_id>', 'xs', self.fetch_xs)
-        self.app.add_url_rule('/api/ys/<int:lc_id>', 'ys', self.fetch_ys)
-        self.app.add_url_rule('/api/coordinates/<int:lc_id>',
-                              'coordinates', self.fetch_coordinates)
-
-        self.preload_aperture_indexes()
-
-    def run(self, *args, **kwargs):
-        logger.info('Listening for connections')
-        self.app.run(*args, **kwargs)
-
-    def extract_data(self):
-        logger.info('Extracting data')
+    def get_lightcurve(self, hdu, lc_id):
         with fitsio.FITS(self.filename) as infile:
-            flux = infile[args.hdu][:, SKIP:]
+            mjd = fetch_from_fits(infile, 'hjd', lc_id)
+            flux = fetch_from_fits(infile, hdu, lc_id)
 
-        logger.debug('Sigma clipping')
-        sc_flux = sigma_clip(flux, axis=1)
-
-        if self.npts_per_bin is not None:
-            sc_flux = self.bin_by(sc_flux)
-
-        logger.debug('Computing statistics')
-        med_flux = np.median(sc_flux, axis=1)
-        mad_flux = np.median(np.abs(sc_flux - med_flux[:, np.newaxis]), axis=1)
-        std_flux = 1.48 * mad_flux
-        frms = std_flux / med_flux
-        return med_flux, frms
-
-    def bin_1d(self, flux, x=None):
-        logger.debug('Binning 1d')
-        x = x if x is not None else np.arange(flux.size)
-        bin_length = int(np.floor(flux.size / self.npts_per_bin))
-        by, be, _ = stats.binned_statistic(x, flux,
-                                           statistic='mean',
-                                           bins=bin_length)
-        return by, (be[:-1] + be[1:]) / 2.
-
-    def bin_by(self, flux, x=None):
-        return fast_bin(flux, self.npts_per_bin)
-
-    def google_xyseries(self, x, y):
-        return list(zip(list(x), list(y)))
-
-    def json_xyseries(self, *args, **kwargs):
-        extra_keys = kwargs.pop('extra_keys', {})
-        out = {'data': self.google_xyseries(*args, **kwargs)}
-        if extra_keys is not None:
-            out.update(extra_keys)
-
-        return jsonify(out)
-
-    def get_lightcurve(self, hdu, index):
-        logger.info('Fetching lightcurve {hdu}:{index}'.format(
-            hdu=hdu, index=index))
-        with fitsio.FITS(self.filename) as infile:
-            mjd = fetch_from_fits(infile, 'hjd', index)
-            flux = fetch_from_fits(infile, hdu, index)
-
-        if self.npts_per_bin is not None:
-            flux, mjd = self.bin_1d(flux, x=mjd)
+        flux, mjd = bin_1d(flux, self.npts_per_bin, x=mjd)
 
         mjd0 = int(mjd.min())
 
@@ -126,100 +90,126 @@ class VisualiseLightcurve(object):
 
         return (mjd - mjd0)[~sc.mask], flux[~sc.mask]
 
-    def preload_aperture_indexes(self):
-        med_flux, frms = self.extract_data()
-        self.ind = np.where((med_flux > 0) & (frms > 0))[0]
-        self.aperture_indexes = np.arange(med_flux.size)
-        return med_flux, frms
+class IndexHandler(BaseHandler):
+    def get(self):
+        self.render('templates/index.html', npts_per_bin=self.npts_per_bin,
+                   render_frms=True)
 
-    def frms(self):
-        med_flux, frms = self.preload_aperture_indexes()
-        return self.json_xyseries(
-            np.log10(med_flux[self.ind].astype(float)),
-            np.log10(frms[self.ind].astype(float)))
+class DetailHandler(BaseHandler):
+    def get(self, lc_id):
+        self.render('templates/view.html', file_index=lc_id,
+                   render_frms=False)
 
-    def get_real_lc_id(self, lc_id):
-        real_lc_id = self.aperture_indexes[self.ind][lc_id]
-        logger.debug('Lightcurve %s => %s', lc_id, real_lc_id)
-        return int(real_lc_id)
+class ObjectIndexHandler(BaseHandler):
+    def real_index(self, i):
+        return int(self.aperture_indexes[self.ind][int(i)])
 
-    def fetch_object_index(self, lc_id):
-        return jsonify({'index': self.get_real_lc_id(lc_id)})
+    def get(self, lc_id):
+        self.write({'index': self.real_index(lc_id)})
 
-    def fetch_lightcurve(self, hdu, lc_id):
-        logger.info('Fetching lightcurve %s', lc_id)
+class FRMSHandler(BaseHandler):
+    def format_frms(self):
+        return {'data': list(zip(
+            list(np.log10(self.med_flux[self.ind].astype(float))),
+            list(np.log10(self.frms[self.ind].astype(float)))))}
+
+    @gen.coroutine
+    def get(self):
+        results = yield executor.submit(self.format_frms)
+        self.write(results)
+
+class LightcurveHandler(BaseHandler):
+    def fetch_data(self, hdu, lc_id):
         mjd, flux = self.get_lightcurve(hdu, lc_id)
+        ind = np.isfinite(flux)
+        return mjd[ind].astype(float), flux[ind].astype(float)
+
+    @gen.coroutine
+    def get(self, hdu, lc_id):
+        mjd, flux = yield executor.submit(self.fetch_data, hdu, lc_id)
         ind = np.isfinite(flux)
         extent = float(flux[ind].ptp())
         frms = float(flux[ind].std() / np.median(flux)) * 1000.
-        return self.json_xyseries(
-            mjd[ind].astype(float), flux[ind].astype(float),
-            extra_keys={
-                'extent': extent,
-                'frms': frms,
-            })
+        self.write({
+            'data': list(zip(mjd, flux)),
+            'extent': extent,
+            'frms': frms,
+        })
 
-    def fetch_x(self, lc_id):
-        logger.info('Fetching x %s', lc_id)
+class MeanCoordinateHandler(BaseHandler):
+    def fetch_coordinate(self, coord_type, lc_id):
         with fitsio.FITS(self.filename) as infile:
-            x = fetch_from_fits(infile, 'ccdx', lc_id)
-        return jsonify({'data': float(np.median(x))})
+            value = fetch_from_fits(
+                infile, 'ccd{coord_type}'.format(coord_type=coord_type), lc_id)
+        return {'data': float(np.median(value))}
 
-    def fetch_y(self, lc_id):
-        logger.info('Fetching y %s', lc_id)
-        with fitsio.FITS(self.filename) as infile:
-            y = fetch_from_fits(infile, 'ccdy', lc_id)
-        return jsonify({'data': float(np.median(y))})
+    @gen.coroutine
+    def get(self, coord_type, lc_id):
+        results = yield executor.submit(
+            self.fetch_coordinate, coord_type, lc_id)
+        self.write(results)
 
-    def fetch_xs(self, lc_id):
-        logger.info('Fetching xs %s', lc_id)
-        with fitsio.FITS(self.filename) as infile:
-            mjd = fetch_from_fits(infile, 'hjd', lc_id)
-            x = fetch_from_fits(infile, 'ccdx', lc_id)
+class CoordinateHandler(BaseHandler):
+    def fetch_coordinate(self, coord_type, lc_id):
+        if coord_type == 'xs':
+            hdu = 'ccdx'
+        elif coord_type == 'ys':
+            hdu = 'ccdy'
+        else:
+            raise RuntimeError("Invalid coordinate type")
 
-        sc = sigma_clip(x)
-        ind = ~sc.mask
-        extent = float(compute_extent(x[ind]))
-        return self.json_xyseries(
-            mjd[ind].astype(float), x[ind].astype(float),
-            extra_keys={'extent': extent})
-
-    def fetch_ys(self, lc_id):
-        logger.info('Fetching ys %s', lc_id)
         with fitsio.FITS(self.filename) as infile:
             mjd = fetch_from_fits(infile, 'hjd', lc_id)
-            y = fetch_from_fits(infile, 'ccdy', lc_id)
+            value = fetch_from_fits(infile, hdu, lc_id)
 
-        sc = sigma_clip(y)
+        sc = sigma_clip(value)
         ind = ~sc.mask
-        extent = float(compute_extent(y[ind]))
-        return self.json_xyseries(
-            mjd[ind].astype(float), y[ind].astype(float),
-            extra_keys={'extent': extent})
+        extent = float(compute_extent(sc[ind]))
+        return {
+            'data': list(zip(mjd[ind].astype(float),
+                             value[ind].astype(float))),
+            'extent': extent,
+        }
 
-    def fetch_binning(self):
-        logger.info('Fetching binning value')
-        return jsonify({'binning': self.npts_per_bin})
+    @gen.coroutine
+    def get(self, coord_type, lc_id):
+        results = yield executor.submit(
+            self.fetch_coordinate, coord_type, lc_id)
+        self.write(results)
 
+class ObjectNameHandler(BaseHandler):
     def fetch_obj_id(self, lc_id):
-        logger.info('Fetching obj_id %s', lc_id)
         with fitsio.FITS(self.filename) as infile:
             cat = infile['catalogue'].read()
 
-        return jsonify({'data': cat['OBJ_ID'][lc_id].decode('utf-8')})
+        return {'data': cat['OBJ_ID'][int(lc_id)].decode('utf-8')}
 
-    def fetch_sysrem_basis_functions(self, basis_id):
-        logger.info('Fetching sysrem basis function %s', basis_id)
+    @gen.coroutine
+    def get(self,  lc_id):
+        results = yield executor.submit(
+            self.fetch_obj_id, lc_id)
+        self.write(results)
+
+class SysremBasisHandler(BaseHandler):
+    def fetch_basis_function(self, basis_id):
+        basis_id = int(basis_id)
+
         with fitsio.FITS(self.filename) as infile:
             imagelist = infile['imagelist'].read()
 
         mjd = imagelist['TMID'][SKIP:]
         aj = imagelist['AJ'].T[basis_id][SKIP:]
 
-        return self.json_xyseries(mjd.astype(float), aj.astype(float))
+        return {'data': list(zip(mjd.astype(float), aj.astype(float)))}
 
+    @gen.coroutine
+    def get(self,  basis_id):
+        results = yield executor.submit(
+            self.fetch_basis_function, basis_id)
+        self.write(results)
+
+class EquatorialCoordinateHandler(BaseHandler):
     def fetch_coordinates(self, lc_id):
-        logger.info('Fetching coordinates %s', lc_id)
         with fitsio.FITS(self.filename) as infile:
             cat_entry = infile['catalogue'][lc_id:lc_id + 1][0]
 
@@ -229,22 +219,49 @@ class VisualiseLightcurve(object):
         ra_hms = coord.ra.to_string()
         dec_dms = coord.dec.to_string()
 
-        return jsonify({'data': {
+        return {'data': {
             'ra': '{ra:.5f}'.format(ra=ra),
             'dec': '{dec:.5f}'.format(dec=dec),
             'ra_full': float(ra),
             'dec_full': float(dec),
             'ra_hms': ra_hms,
-            'dec_dms': dec_dms}})
+            'dec_dms': dec_dms}}
 
-    def index(self):
-        return render_template('index.html', render_frms=True)
+    @gen.coroutine
+    def get(self, lc_id):
+        lc_id = int(lc_id)
+        results = yield executor.submit(self.fetch_coordinates, lc_id)
+        self.write(results)
 
-    def show_object(self, lc_id):
-        return render_template('view.html', file_index=lc_id,
-                              render_frms=False)
 
+def construct_application(args, ind, med_flux, frms, aperture_indexes):
+    url_mapping = [
+        (r'/', IndexHandler),
+        (r'/view/([0-9]+)', DetailHandler),
+        # API
+        (r'/api/object_index/([0-9]+)', ObjectIndexHandler),
+        (r'/api/data', FRMSHandler),
+        (r'/api/lc/([a-z]+)/([0-9]+)', LightcurveHandler),
+        (r'/api/([xy])/([0-9]+)', MeanCoordinateHandler),
+        (r'/api/([xy]s)/([0-9]+)', CoordinateHandler),
+        (r'/api/obj_id/([0-9]+)', ObjectNameHandler),
+        (r'/api/sysrem_basis/([0-9]+)', SysremBasisHandler),
+        (r'/api/coordinates/([0-9]+)', EquatorialCoordinateHandler),
+    ]
 
+    constructor_params = {
+        'args': args,
+        'aperture_indexes': aperture_indexes,
+        'ind': ind,
+        'frms': frms,
+        'med_flux': med_flux,
+    }
+
+    application = tornado.web.Application([
+        (route, handler, constructor_params)
+        for (route, handler) in url_mapping
+    ], static_path='static', debug=args.debug)
+    return application
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -262,9 +279,17 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true',
                         help='Run the debug server')
     args = parser.parse_args()
+
     if args.verbose:
         logger.setLevel('DEBUG')
     logger.debug(args)
+
     logger.info('Skipping the first %s points', SKIP)
-    VisualiseLightcurve(args).run(host=args.host, port=args.port,
-                                  debug=args.debug)
+    ind, med_flux, frms = extract_data(args.filename, npts_per_bin=args.bin)
+    aperture_indexes = np.arange(med_flux.size)
+
+    logger.info('Application listening on %s:%s', args.host, args.port)
+    application = construct_application(args, ind, med_flux, frms,
+                                        aperture_indexes)
+    application.listen(args.port, address=args.host)
+    tornado.ioloop.IOLoop().current().start()
